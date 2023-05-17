bad2de42fdabcdba7496eb64656a44d4ea418b99
Alex Riasanovsky <a.riasanovsky@gmail.com>
2023-05-17 17:01:38 -0400

    init commit w/ working code

---
+++ README.md
@@ -0,0 +1,37 @@
+# Java RNG reader
+
+A project to build tests for the [`libgdx-xs128`](https://crates.io/crates/libgdx-xs128).
+
+## Assumptions
+
+You have `gradle` and the `Java Development Kit` installed.
+
+## Usage
+
+Run this application with a seed followed by up to `16` arguments.
+Arguments can be
+
+- a positive integer (which will yield a `CappedU64` result),
+- a non-positive number (which will yield a `U64` result),
+- or a string starting with `'a'` followed by a number (which will yield an `Advance` result).
+
+The remaining values in the 16-value array will be `None`.
+
+For example,
+
+```bash
+gradle run --args="12345 0 a10 134 -1"
+```
+
+creates a random number generator with seed `12345`, produces one `u64`, advances `10` times, generates a `u64` modulo `134`, then another `u64`, then nothing.
+
+It prints
+
+```rust
+RngValues {
+    seed: 12345,
+    values: [Some(RngValue::U64(1382432690769144372i64 as u64)), Some(RngValue::Advance(10)), Some(RngValue::CappedU64 { modulus: 134, residue: 83 }), Some(RngValue::U64(-5355119237153046436i64 as u64)), None, None, None, None, None, None, None, None, None, None, None, None, ],
+}
+```
+
+to the terminal.
---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+import com.badlogic.gdx.math.RandomXS128;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Usage: seed [a#|n1 n2 ... n16]");
+            System.exit(1);
+        }
+
+        // Parse command-line arguments
+        long seed = Long.parseLong(args[0]);
+        String[] a = new String[args.length - 1];
+        System.arraycopy(args, 1, a, 0, a.length);
+
+        // Initialize RNG
+        RandomXS128 rng = new RandomXS128();
+        rng.setSeed(seed);
+
+        // Print seed
+        System.out.println("RngValues {");
+        System.out.println("    seed: " + seed + ",");
+
+        // Print values
+        System.out.print("    values: [");
+        for (int i = 0; i < a.length; i++) {
+            if (a[i].startsWith("a")) {
+                System.out.print("Some(RngValue::Advance(" + a[i].substring(1) + ")), ");
+                for (int j = 0; j < Long.parseLong(a[i].substring(1)); j++) {
+                    rng.nextLong();
+                }
+            } else {
+                long ai = Long.parseLong(a[i]);
+                if (ai > 0) {
+                    long result = rng.nextLong(ai);
+                    System.out.print("Some(RngValue::CappedU64 { modulus: " + ai + ", residue: " + result + " }), ");
+                } else {
+                    System.out.print("Some(RngValue::U64(" + rng.nextLong() + "i64 as u64)), ");
+                }
+            }
+        }
+
+        // Fill rest of the array with None
+        for (int i = a.length; i < 16; i++) {
+            System.out.print("None, ");
+        }
+
+        System.out.println("],");
+        System.out.println("}");
+    }
+}
